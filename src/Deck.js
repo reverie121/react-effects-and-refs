9e1ea10577718f79fab711fa2506f3a2cef60864
Jeremy Sobczak <jeremy@jeremys-mbp.lan>
2023-06-14 11:07:26 -0400

    Button now starts/stops drawing a card every second until the deck runs out. Part 2 completed.

--- src/Deck.js
+++ src/Deck.js
@@ -6,9 +6,12 @@ import "./Deck.css";
 const Deck = () => {
     const deckID = useRef();
     const cardsRemaining = useRef();
+    const timerID = useRef();
 
     const INITIAL_STATE = [];
-    const [ cards, setCards] = useState(INITIAL_STATE);
+    const [ cards, setCards ] = useState(INITIAL_STATE);
+
+    const [ isDrawing, setIsDrawing ] = useState(false);
 
     // this is called *after* component first added to DOM
     useEffect(function getDeckOfCards() {
@@ -17,30 +20,56 @@ const Deck = () => {
                 "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
                 deckID.current = result.data.deck_id;
                 cardsRemaining.current = result.data.remaining;
-                console.log(cardsRemaining.current);
-
             }
         queryForDeck();
     }, []);
 
-    // useEffect(function drawCard() {
-        async function queryForCard() {
-            if (cardsRemaining.current === 0) {
-                return alert('Error: no cards remaining!');
-            }
-            const result = await axios.get(
-                `https://deckofcardsapi.com/api/deck/${deckID.current}/draw/?count=1`);
+    /* Draw one card every second if autoDraw is true */
+    useEffect(() => {
+        /* Draw a card via API, add card to state "drawn" list */
+        async function getCard() {
+
+            const result = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID.current}/draw/`);
+
             cardsRemaining.current = result.data.remaining;
-            console.log(cardsRemaining.current);
+            if (cardsRemaining.current === 0) {
+                setIsDrawing(false);
+            }
             const { value, suit, image } = result.data.cards[0];
-            const cardsPlusDraw = [...cards, { value, suit, image, key: image }];
-            setCards(cardsPlusDraw);
+            setCards(cards => [
+                ...cards,
+                {
+                value, suit, image, key: image
+                }
+            ]);
+        }
+
+        if (isDrawing && !timerID.current) {
+            timerID.current = setInterval(async () => {
+            await getCard();
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(timerID.current);
+            timerID.current = null;
         };
-    // }, []);
+    }, [isDrawing, setIsDrawing, cards]);
+
+    const toggle = () => {
+        if (cardsRemaining.current !== 0) {
+            setIsDrawing(current => !current);
+        }
+        else {
+            alert('No cards remaining!')
+        }
+    };
 
     return (
         <div className='DeckContainer'>
-            <button onClick={queryForCard}>Draw!</button>
+            <button onClick={toggle}>
+                {isDrawing ? "Stop Drawing" : "Start Drawing"}
+            </button>
             <div className='Deck'>
                 {cards.map(card => <Card 
                     value={card.value}
